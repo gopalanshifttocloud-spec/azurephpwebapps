0e3b942f30616d9fd7c70324da47698380b58e4c
Gopalan <gopalan.shifttocloud@gmail.com>
2026-03-17 17:23:33 +0530

    index page -azure db changes

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 test index php files
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:azuresqlserversigsync.database.windows.net,1433; Database = free-sql-db-9705890", "CloudSAcb231d5e", "admin@123456");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    exit(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAcb231d5e", "pwd" => "admin@123456", "Database" => "free-sql-db-9705890", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:azuresqlserversigsync.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$database = "free-sql-db-9705890";
+$username = "adminuser";          // Server admin login
+$password = "admin@123456"; // Reset password
+
+try {
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
