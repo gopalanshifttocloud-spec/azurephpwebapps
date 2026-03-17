6ac3d7801818319c89cf698a72fff76f4ab328e3
Gopalan <gopalan.shifttocloud@gmail.com>
2026-03-17 17:21:42 +0530

    index page -azure db changes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@ test index php files
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:azuresqlserversigsync.database.windows.net,1433; Database = free-sql-db-9705890", "CloudSAcb231d5e", "TlA100%Fe");
+    $conn = new PDO("sqlsrv:server = tcp:azuresqlserversigsync.database.windows.net,1433; Database = free-sql-db-9705890", "CloudSAcb231d5e", "admin@123456");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -11,7 +11,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAcb231d5e", "pwd" => "TlA100%Fe", "Database" => "free-sql-db-9705890", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "CloudSAcb231d5e", "pwd" => "admin@123456", "Database" => "free-sql-db-9705890", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:azuresqlserversigsync.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
